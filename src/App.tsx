30a183b55b6ccf284eb2141eaf0e041ca01e29e0
talerqa <hitri3003@gmail.com>
2023-10-22 16:32:42 +0300

    add: table in app with data from server

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,58 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import './App.module.scss'
+import {TypeData, useGetAssetsQuery} from "@/servicies/baseApi.ts";
+import {Table} from "@/table.tsx";
+
 
 function App() {
-  const [count, setCount] = useState(0)
+
+  const {data} = useGetAssetsQuery()
+
+
+  const {Root, Head, Body, Row, Cell} = Table
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <Root>
+        <Head>
+          <Row>
+            <Cell>rank</Cell>
+            <Cell>name</Cell>
+            <Cell>symbol</Cell>
+            <Cell>priceUsd</Cell>
+            <Cell>marketCapUsd</Cell>
+            <Cell>supply</Cell>
+            <Cell>volumeUsd24Hr</Cell>
+            <Cell>changePercent24Hr</Cell>
+            <Cell>vwap24Hr</Cell>
+          </Row>
+        </Head>
+        <Body>
+
+
+          {data?.data.map((item: TypeData) => {
+            return <Row>
+              <Cell><p>{item.rank}</p></Cell>
+              <Cell><img
+                src={`https://assets.coincap.io/assets/icons/${item.symbol.toLowerCase()}@2x.png`}
+                alt=""/>
+                <a
+                  href={`https://coinmarketcap.com/currencies/${item.id}/`}
+                  target='_blank'>
+
+                  <p>{item.name}</p>
+                </a></Cell>
+              <Cell><p>{item.symbol}</p></Cell>
+              <Cell><p>{item.priceUsd}</p></Cell>
+              <Cell><p>{item.marketCapUsd}</p></Cell>
+              <Cell><p>{item.supply}</p></Cell>
+              <Cell><p>{item.volumeUsd24Hr}</p></Cell>
+              <Cell><p>{item.changePercent24Hr}</p></Cell>
+              <Cell><p>{item.vwap24Hr}</p></Cell>
+            </Row>
+          })}
+
+        </Body>
+      </Root>
     </>
   )
 }
