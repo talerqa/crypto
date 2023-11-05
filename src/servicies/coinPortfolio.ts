611587dfa41585e060f9787f16128439de4fd370
talerqa <hitri3003@gmail.com>
2023-11-05 16:30:40 +0300

    fix: portfolio

--- src/servicies/coinPortfolio.ts
+++ src/servicies/coinPortfolio.ts
@@ -1,19 +1,17 @@
 import {createSlice, PayloadAction} from "@reduxjs/toolkit";
-import {TypeData, TypeDataInPortfolio} from "@/servicies/baseApi.type.ts";
+import {TypeDataInPortfolio} from "@/servicies/baseApi.type.ts";
 
 const initialState: Array<TypeDataInPortfolio> = JSON.parse(localStorage.getItem('value') || '{}').length ? JSON.parse(localStorage.getItem('value') || '{}') : []
 
-
-console.log( JSON.parse(localStorage.getItem('value')))
 export const slice = createSlice({
   name: 'coinPortfolio',
   initialState,
   reducers: {
-    addCoinInPortfolio: (state, action: PayloadAction<{
-      coin: TypeData[],
-      valueOfCoin: string | number
-    }>) => {
-
+    addCoinInPortfolio: (state, action: PayloadAction<any>) => {
+      // {
+      //   coin: TypeData[],
+      //     valueOfCoin?: string | number | null
+      // }
       state.push({
         ...action.payload.coin[0],
         valueOfCoin: Number(action.payload.valueOfCoin)
